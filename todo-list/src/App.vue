ef788d14837e19e3a65bd9eaa8bd111556e59d9b
FelicityAmiao <1760077814@qq.com>
2019-07-20 11:54:44 +0800

    feat: show todo list

---
+++ todo-list/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div>
+      <p>Jquery To Do List</p>
+      <p>Simple Todo List with adding and filter by diff status.</p>
+      <p>
+        <span><input type="text" /></span>
+        <span><button>Add</button></span>
+      </p>
+      <ul>
+        <li v-for="(item, index) in todoItemCount" v-bind:key="index">
+          <todo-list-item v-bind:itemName="taskName"></todo-list-item>
+        </li>
+      </ul>
+      <p>
+        <span>ALL </span>
+        <span>Active </span>
+        <span>complete </span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoListItem from "./components/TodoListItem.vue"
+export default {
+  name: 'app',
+  data: function() {
+    return {
+      todoItemCount: 1,
+      taskName: 'default task name'
+    }
+  },
+  components: {
+    TodoListItem
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ todo-list/src/components/TodoListItem.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <input type="checkbox" /> {{itemName}}
+    </div>
+</template>
+<script>
+export default {
+    name: "TodoListItem",
+    props: ["itemName"],
+}
+</script>
+
