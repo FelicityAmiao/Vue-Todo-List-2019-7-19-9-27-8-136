0e955fb7ae1ae17276ab3d177d1f01ca88cdcfb2
FelicityAmiao <1760077814@qq.com>
2019-07-21 09:40:51 +0800

    feat: import self-defined css for todolist

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
-    <div>
-      <p>Jquery To Do List</p>
-      <p>Simple Todo List with adding and filter by diff status.</p>
-      <p>
-        <span><input type="text" /></span>
-        <span><button>Add</button></span>
+    <div class="mainPanel">
+      <h2>Jquery To Do List</h2>
+      <p class="briefIntro">
+        <em>Simple Todo List with adding and filter by diff status.</em>
       </p>
-      <ol>
+      <div>
+        <span><input type="text" class="inputText"/></span>
+        <div class="addButton">Add</div>
+      </div>
+      <br>
+      <ol class="todoList">
         <li v-for="(item, index) in todolist" v-bind:key="index">
           <todo-list-item v-bind:itemName="item.taskName"></todo-list-item>
         </li>
       </ol>
-      <p>
-        <span>ALL </span>
-        <span>Active </span>
-        <span>complete </span>
-      </p>
+      <div>
+        <ul class="filters">
+          <li>ALL</li>
+          <li>Active</li>
+          <li>Complete</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
