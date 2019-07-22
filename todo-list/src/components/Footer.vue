077c0b431d26f2d4a8f219aa99256282d402418a
FelicityAmiao <1760077814@qq.com>
2019-07-22 23:11:41 +0800

    fix: add three button clicked class

--- todo-list/src/components/Footer.vue
+++ todo-list/src/components/Footer.vue
@@ -1,21 +1,20 @@
 <template>
   <div>
     <ul class="filters">
-      <li v-on:click="selectAllItems">ALL</li>
-      <li v-on:click="selectActiveItems">Active</li>
-      <li v-on:click="selectCompleteItems">Complete</li>
+      <li v-on:click="selectAllItems" :class="{filterButton: isAllClicked}">ALL</li>
+      <li v-on:click="selectActiveItems" :class="{filterButton: isActiveClicked}">Active</li>
+      <li v-on:click="selectCompleteItems" :class="{filterButton: isCompleteClicked}">Complete</li>
     </ul>
   </div>
 </template>
 <script>
 export default {
   name: "Footer",
-  props: {
-    showType: String
-  },
   data() {
     return {
-      listType: this.showType
+      isAllClicked: true,
+      isActiveClicked: false,
+      isCompleteClicked: false
     }
   },
   methods: {
@@ -23,18 +22,27 @@ export default {
       const filterTodolistItem = (list) => {
         return list;
       }
+      this.isAllClicked = true;
+      this.isActiveClicked = false;
+      this.isCompleteClicked = false;
       this.$emit('filter-todolist-item', filterTodolistItem);
     },
     selectActiveItems: function() {
       const filterTodolistItem = (list) => {
         return list.filter(item => !item.checked);
       }
+      this.isActiveClicked = true;
+      this.isAllClicked = false;
+      this.isCompleteClicked = false;
       this.$emit('filter-todolist-item', filterTodolistItem);
     },
     selectCompleteItems: function() {
       const filterTodolistItem = (list) => {
         return list.filter(item => item.checked);
       }
+      this.isAllClicked = false;
+      this.isActiveClicked = false;
+      this.isCompleteClicked = true;
       this.$emit('filter-todolist-item', filterTodolistItem);
     }
   }
